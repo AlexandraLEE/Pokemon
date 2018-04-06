6473541685cea5c04d5c6305c220738df6f8e680
Alexandra LEE <palexalee@gmail.com>
2018-04-06 12:21:22 +0200

    "adding"

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npm run build -- --base-href "https://alexandralee.github.io/SuperAxelAlexandra/"
+ng build --base-href "https://alexandralee.github.io/SuperAxelAlexandra/"
 npm run deploy -- --repo=https://github.com/AlexandraLEE/SuperAxelAlexandra.git
